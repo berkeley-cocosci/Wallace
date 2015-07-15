34a42b5151853a5c26a2440e2a8c7be06b5c7ad1
Jordan Suchow <suchow@post.harvard.edu>
2015-07-14 23:15:32 -0700

    Improve style and commenting

--- tests/_test_bartlett.py
+++ tests/_test_bartlett.py
@@ -1,38 +1,49 @@
-# from wallace import nodes, information, db, models
-# from wallace.information import Meme, Gene
-# from nose.tools import raises
-# import subprocess
-# import re
-# import requests
+"""Integration tests for Bartlett1932."""
+
+import subprocess
+import re
+import requests
 
 
-# class TestBartlett(object):
+class TestBartlett(object):
 
-#     sandbox_output = subprocess.check_output("cd examples/bartlett1932; wallace sandbox", shell=True)
-#     exp_id = re.search('Running as experiment (.*)...', sandbox_output).group(1)
-#     exp_address = "http://" + exp_id + ".herokuapp.com"
+    # Launch the experiment in the sandbox.
+    sandbox_output = subprocess.check_output(
+        "cd examples/bartlett1932; wallace sandbox",
+        shell=True)
 
-#     subprocess.call("cd examples/bartlett1932; wallace logs --app " + exp_id, shell=True)
+    m = re.search('Running as experiment (.*)...', sandbox_output)
+    exp_id = m.group(1)
+    url = "http://" + exp_id + ".herokuapp.com"
 
-#     args = {'hitId': 'bartlett-test-hit', 'assignmentId': 1, 'workerId': 1, 'mode': 'sandbox'}
-#     participant = requests.get(exp_address + '/exp', params=args)
-#     #print participant.text
+    # Open the logs in the browser.
+    subprocess.call(
+        "cd examples/bartlett1932; wallace logs --app " + exp_id,
+        shell=True)
 
-#     args = {'Event.1.EventType': 'AssignmentAccepted', 'Event.1.AssignmentId': 1}
-#     notification = requests.post(exp_address + '/notifications', data=args)
+    # Simulate participants.
+    args = {'hitId': 'bartlett-test-hit', 'assignmentId': 1, 'workerId': 1, 'mode': 'sandbox'}
+    participant = requests.get(url + '/exp', params=args)
+    #print participant.text
 
-#     working = True
-#     while working is True:
-#         args = {'unique_id': '1:1'}
-#         agent = requests.post(exp_address + '/agents', data=args)
-#         working = agent.status_code == 200
-#         if working is True:
-#             agent_id = agent.json()['agents']['id']
-#             args = {'destination_id': agent_id}
-#             transmission = requests.get(exp_address + '/transmissions', data=args)
-#             info = requests.get(exp_address + '/information/' + str(transmission.json()['transmissions'][0]['info_id']), data=args)
-#             args = {'origin_id': agent_id, 'contents': 'test test test', 'info_type': 'base'}
-#             requests.post(exp_address + '/information', data=args)
+    args = {
+        'Event.1.EventType': 'AssignmentAccepted',
+        'Event.1.AssignmentId': 1
+    }
+    notification = requests.post(url + '/notifications', data=args)
 
-#     #subprocess.call("heroku apps:destroy --app " + exp_id + " --confirm " + exp_id, shell=True)
-#     # for app in $(heroku apps | sed '/[pt]/ d'); do heroku apps:destroy --app $app --confirm $app; done
+    working = True
+    while working is True:
+        args = {'unique_id': '1:1'}
+        agent = requests.post(url + '/agents', data=args)
+        working = agent.status_code == 200
+        if working is True:
+            agent_uuid = agent.json()['agents']['uuid']
+            args = {'destination_uuid': agent_uuid}
+            transmission = requests.get(url + '/transmissions', data=args)
+            info = requests.get(url + '/information/' + str(transmission.json()['transmissions'][0]['info_uuid']), data=args)
+            args = {'origin_uuid': agent_uuid, 'contents': 'test test test', 'info_type': 'base'}
+            requests.post(url + '/information', data=args)
+
+    #subprocess.call("heroku apps:destroy --app " + exp_id + " --confirm " + exp_id, shell=True)
+    # for app in $(heroku apps | sed '/[pt]/ d'); do heroku apps:destroy --app $app --confirm $app; done
