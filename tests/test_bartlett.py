4009e173b7f6ade738ae1efd0e3cd609b2c25de7
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2015-07-15 09:01:20 -0700

    make test workers threaded

--- tests/test_bartlett.py
+++ tests/test_bartlett.py
@@ -3,9 +3,13 @@
 import subprocess
 import re
 import requests
+import threading
 
 
 class TestBartlett(object):
+
+    # How many parallel simulated workers do you want
+    autobots = 1
 
     # Launch the experiment in the sandbox.
     sandbox_output = subprocess.check_output(
@@ -21,29 +25,47 @@ class TestBartlett(object):
         "cd examples/bartlett1932; wallace logs --app " + exp_id,
         shell=True)
 
-    # Simulate participants.
-    args = {'hitId': 'bartlett-test-hit', 'assignmentId': 1, 'workerId': 1, 'mode': 'sandbox'}
-    participant = requests.get(url + '/exp', params=args)
-    #print participant.text
+    # methods that defines the behavior of each worker
+    def autobot(url):
+        # create participant
+        args = {'hitId': 'bartlett-test-hit', 'assignmentId': 1, 'workerId': 1, 'mode': 'sandbox'}
+        requests.get(url + '/exp', params=args)
 
-    args = {
-        'Event.1.EventType': 'AssignmentAccepted',
-        'Event.1.AssignmentId': 1
-    }
-    notification = requests.post(url + '/notifications', data=args)
+        # send AssignmentAccepted notification
+        args = {
+            'Event.1.EventType': 'AssignmentAccepted',
+            'Event.1.AssignmentId': 1
+        }
+        requests.post(url + '/notifications', data=args)
 
-    working = True
-    while working is True:
-        args = {'unique_id': '1:1'}
-        agent = requests.post(url + '/agents', data=args)
-        working = agent.status_code == 200
-        if working is True:
-            agent_uuid = agent.json()['agents']['uuid']
-            args = {'destination_uuid': agent_uuid}
-            transmission = requests.get(url + '/transmissions', data=args)
-            info = requests.get(url + '/information/' + str(transmission.json()['transmissions'][0]['info_uuid']), data=args)
-            args = {'origin_uuid': agent_uuid, 'contents': 'test test test', 'info_type': 'base'}
-            requests.post(url + '/information', data=args)
+        # work through the trials
+        working = True
+        while working is True:
+            args = {'unique_id': '1:1'}
+            agent = requests.post(url + '/agents', data=args)
+            working = agent.status_code == 200
+            if working is True:
+                agent_uuid = agent.json()['agents']['uuid']
+                args = {'destination_uuid': agent_uuid}
+                transmission = requests.get(url + '/transmissions', data=args)
+                requests.get(url + '/information/' + str(transmission.json()['transmissions'][0]['info_uuid']), data=args)
+                args = {'origin_uuid': agent_uuid, 'contents': 'test test test', 'info_type': 'base'}
+                requests.post(url + '/information', data=args)
+
+        # send AssignmentSubmitted notification
+        args = {
+            'Event.1.EventType': 'AssignmentSubmitted',
+            'Event.1.AssignmentId': 1
+        }
+        requests.post(url + '/notifications', data=args)
+        return
+
+    # create worker threads
+    threads = []
+    for i in range(autobots):
+        t = threading.Thread(target=autobot, args=(url, ))
+        threads.append(t)
+        t.start()
 
     #subprocess.call("heroku apps:destroy --app " + exp_id + " --confirm " + exp_id, shell=True)
     # for app in $(heroku apps | sed '/[pt]/ d'); do heroku apps:destroy --app $app --confirm $app; done
